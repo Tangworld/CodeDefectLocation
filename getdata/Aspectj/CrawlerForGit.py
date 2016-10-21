379af147264382dabde610d6e6768b5132403faa
Tangworld <948138224@qq.com>
2016-10-21 20:59:23 +0800

    尝试爬取sourcefile

--- getdata/Aspectj/CrawlerForGit.py
+++ getdata/Aspectj/CrawlerForGit.py
@@ -8,9 +8,9 @@ import MySQLdb
 from bs4 import BeautifulSoup
 
 
-def getAllLinks():
+def getAllLinks(ofs):
     prefix = "https://git.eclipse.org"
-    begin = 'https://git.eclipse.org/c/aspectj/org.aspectj.git/log/'
+    begin = 'https://git.eclipse.org/c/aspectj/org.aspectj.git/log/?ofs='+str(ofs)
     html = urllib.urlopen(begin).read()
     soup = BeautifulSoup(html, 'html.parser')
     trs = soup.find(name='tr', attrs={"class": re.compile(r'nohover')}).next_siblings
@@ -26,6 +26,16 @@ def getAllLinks():
             page = tr.td.next_sibling.a["href"]
             url = prefix + page
             print url
+            FILEPATH = getFile(url, BUGID)
+            db = MySQLdb.connect("localhost", "root", "root", "CodeDefectLocation")
+            cursor = db.cursor()
+            cursor.execute(
+                "INSERT INTO assist(bugid, sourcepath, author) VALUES ('%s', '%s', '%s')" % (
+                BUGID, FILEPATH, AUTHOR))
+            db.commit()
+            cursor.close()
+            db.close()
+
 
 def getBugid(line):
     num = ""
@@ -33,5 +43,27 @@ def getBugid(line):
         if char.isdigit():
             num += char
     return num
+
+def getFile(url,bugid):
+    path = "/home/tsj/PycharmProjects/CodeDefectLocation/getdata/Aspectj/SourceFile/"
+    html = urllib.urlopen(url).read()
+    soup = BeautifulSoup(html, 'html.parser')
+    table = soup.find(name='table', attrs={"class": re.compile(r'^diff$')})
+    contents = table.tr.td.children
+    print contents
+    filename = path + bugid + ".txt"
+    f = file(filename, "a+")
+    for content in contents:
+        # print content.string
+        if content.string is None:
+            continue
+        print type(content.string)
+        f.write(content.string.encode("utf-8")+"\n")
+    f.close()
+    return filename
+
 if __name__ == '__main__':
-    getAllLinks()
+    i = 450
+    while i < 1000:
+        getAllLinks(i)
+        i += 50
