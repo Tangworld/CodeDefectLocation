b6228c7bd5a69e013d09993609eee8c8619c2869
Tangworld <948138224@qq.com>
2016-10-22 19:14:00 +0800

    添加了去除重复数据的函数

--- getdata/Aspectj/UpdateData.py
+++ getdata/Aspectj/UpdateData.py
@@ -28,5 +28,36 @@ def update():
     cursor.close()
     db.close()
 
+def minor():
+    db = MySQLdb.connect("localhost", "root", "root", "CodeDefectLocation")
+    cursor = db.cursor()
+    cursor.execute("select * from assist")
+    dataset = cursor.fetchall()
+    cursor.close()
+    db.close()
+    dataset = list(dataset)
+    for i in range(0, 2480, 1):
+        dataset[i] = list(dataset[i])
+    for i in range(0, 2480, 1):
+        j = i+1
+        while j < 2480:
+            # print dataset[j][1]
+            if dataset[j][1] == dataset[i][1]:
+                # print dataset[j]
+                dataset[j][1] = "000000"
+            j += 1
+    print dataset.__len__()
+    # for i in range(0,2480,1):
+    #     print dataset[i]
+    for i in range(0, 2480, 1):
+        if dataset[i][1] == "000000":
+            db = MySQLdb.connect("localhost", "root", "root", "CodeDefectLocation")
+            cursor = db.cursor()
+            cursor.execute("update assist set bugid = '000000' where id='%s'" % dataset[i][0])
+            db.commit()  # Commit the transaction
+            cursor.close()
+            db.close()
+
+
 if __name__ == "__main__":
-    update()
+    minor()
