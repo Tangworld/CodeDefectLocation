93b9151d5690abfa8820706a28ce5bf914572599
Tangworld <948138224@qq.com>
2017-08-02 14:53:18 +0800

    20170802证明两种方法用的数据相同

--- getdata/Aspectj-Large/l2ss/dealData.py
+++ getdata/Aspectj-Large/l2ss/dealData.py
@@ -2,32 +2,47 @@
 import MySQLdb
 
 def main():
-    input = open("aspectj.txt")
+    input = open("../data/l2ss/aspectj.txt")
     lines = input.readlines()
-    db = MySQLdb.connect("localhost", "root", "root", "locator")
-    cursor = db.cursor()
-
+    input.close()
+    bugidmap = open('../data/bughunter/BugidMap.txt')
+    d_lines = bugidmap.readlines()
+    bugidmap.close()
+    # db = MySQLdb.connect("localhost", "root", "root", "locator")
+    # cursor = db.cursor()
+    dictionary = []
+    for d in d_lines:
+        d = d.replace('\n', '')
+        dictionary.append(d)
     cnt = 0
 
-    for line in lines:
-        temp = eval(line)
-        priority = str(temp['pr'])
-        opendate = str(temp['tm'])
-        reporter = temp['rp']
-        component = str(temp['cm'])
-        tr = str(temp['tr'])
-        fileloc = str(temp['ts'])
-        print reporter
-        severity = str(temp['sv'])
-        version = str(temp['vs'])
-        keywords = str(temp['ws'])
-        platform = str(temp['pf'])
-        os = str(temp['os'])
+    for i in range(0, 1000):
+        temp = eval(lines[i])
         bugid = str(temp['id'])
+        if bugid in dictionary:
+            print bugid
+            cnt += 1
+    # for line in lines:
+    #     temp = eval(line)
+    #     priority = str(temp['pr'])
+    #     opendate = str(temp['tm'])
+    #     reporter = temp['rp']
+    #     component = str(temp['cm'])
+    #     tr = str(temp['tr'])
+    #     fileloc = str(temp['ts'])
+    #     # print reporter
+    #     severity = str(temp['sv'])
+    #     version = str(temp['vs'])
+    #     keywords = str(temp['ws'])
+    #     platform = str(temp['pf'])
+    #     os = str(temp['os'])
+    #     bugid = str(temp['id'])
         #cursor.execute("INSERT INTO Report(reporter,opendate,fileloc,component,version,platform,os,priority,severity,bugid,keywords)"
         #               " VALUES("+reporter+","+opendate+","+fileloc+","+component+","+version+","+platform+","+os+","+priority+","+severity+","+bugid+","+keywords+")")
-        print bugid
-        cnt += 1
+
+    # db.commit()  # Commit the transaction
+    # cursor.close()
+    # db.close()
     print cnt
 
 if __name__ == "__main__":
