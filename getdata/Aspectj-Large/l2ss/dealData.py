d1e92cb98e891d5b7ea1eb871f6df8eb4f930b3a
Tangworld <948138224@qq.com>
2017-08-02 15:32:10 +0800

    20170802重整了数据，目前有886条，待修改字段

--- getdata/Aspectj-Large/l2ss/dealData.py
+++ getdata/Aspectj-Large/l2ss/dealData.py
@@ -8,41 +8,40 @@ def main():
     bugidmap = open('../data/bughunter/BugidMap.txt')
     d_lines = bugidmap.readlines()
     bugidmap.close()
-    # db = MySQLdb.connect("localhost", "root", "root", "locator")
-    # cursor = db.cursor()
+
+    db = MySQLdb.connect("localhost", "root", "root", "locator")
+    db.set_character_set('utf8')
+    cursor = db.cursor()
+
     dictionary = []
     for d in d_lines:
         d = d.replace('\n', '')
         dictionary.append(d)
     cnt = 0
-
-    for i in range(0, 1000):
-        temp = eval(lines[i])
+    for line in lines:
+        temp = eval(line)
+        priority = str(temp['pr'])
+        opendate = str(temp['tm'])
+        reporter = temp['rp']
+        component = str(temp['cm'])
+        tr = str(temp['tr'])
+        fileloc = str(temp['ts'])
+        # print reporter
+        severity = str(temp['sv'])
+        version = str(temp['vs'])
+        keywords = str(temp['ws'])
+        platform = str(temp['pf'])
+        os = str(temp['os'])
         bugid = str(temp['id'])
         if bugid in dictionary:
-            print bugid
+            print reporter, opendate, component, version, platform, os, priority, severity,bugid
             cnt += 1
-    # for line in lines:
-    #     temp = eval(line)
-    #     priority = str(temp['pr'])
-    #     opendate = str(temp['tm'])
-    #     reporter = temp['rp']
-    #     component = str(temp['cm'])
-    #     tr = str(temp['tr'])
-    #     fileloc = str(temp['ts'])
-    #     # print reporter
-    #     severity = str(temp['sv'])
-    #     version = str(temp['vs'])
-    #     keywords = str(temp['ws'])
-    #     platform = str(temp['pf'])
-    #     os = str(temp['os'])
-    #     bugid = str(temp['id'])
-        #cursor.execute("INSERT INTO Report(reporter,opendate,fileloc,component,version,platform,os,priority,severity,bugid,keywords)"
-        #               " VALUES("+reporter+","+opendate+","+fileloc+","+component+","+version+","+platform+","+os+","+priority+","+severity+","+bugid+","+keywords+")")
+            cursor.execute("INSERT INTO locator_report(reporter,opendate,component,version,platform,os,priority,severity,bugid)"
+                        " VALUES('"+reporter+"','"+opendate+"','"+component+"','"+version+"','"+platform+"','"+os+"','"+priority+"','"+severity+"','"+bugid+"')")
 
-    # db.commit()  # Commit the transaction
-    # cursor.close()
-    # db.close()
+    db.commit()  # Commit the transaction
+    cursor.close()
+    db.close()
     print cnt
 
 if __name__ == "__main__":
